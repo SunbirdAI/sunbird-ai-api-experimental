cb1524124a09d3b88eddc20133a51f688f4552a5
ConradKash <kakurucon1234@gmail.com>
2023-08-30 11:09:34 +0300

    Adding tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
-from utils.api import app
+from app.utils.api import app
 
 client = TestClient(app)
+
+
 def test_root_api():
     response = client.get('/')
     assert response.status_code == 200
