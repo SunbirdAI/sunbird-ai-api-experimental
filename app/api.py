9414f4bad47fe26d84edeca3f8e8d7f65ff330aa
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2023-08-31 18:15:54 +0300

    Add: file translate endpoint
    Rebase imp-file-translation branch

--- app/api.py
+++ app/api.py
@@ -3,6 +3,11 @@ from app.schemas.tasks import TranslationRequest, TranslationResponse
 from app.inference_services.translate import (translate_text,
                                               long_text_translation,
                                               predicted_language)
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile, Form
+
+from app.file_translate.utils import parse_filename, validate_uploaded_file, \
+    extract_txt_frm_upload, create_txt_file, generate_translated_file
 
 
 app = FastAPI()
@@ -30,3 +35,33 @@ def translate(translation_request: TranslationRequest):
                                          translation_request.target_language)
 
     return TranslationResponse(text=response, source_language=source_language)
+# file translate endpoint
+@app.post("/upload")
+async def upload_file(
+    file: Annotated[UploadFile, File()],
+    trans_lang: Annotated[str, Form()],
+    src_lang: Annotated[str | None, Form()] = None
+):
+
+    filename, file_type = parse_filename(file.filename)
+    validate_uploaded_file(file_type=file_type, file_size=file.size)
+
+    content = await file.read()
+    text = extract_txt_frm_upload(
+        content=content, filename=filename, file_type=file_type)
+    # can create txt file with uploaded text
+    create_txt_file(text=text, filename=filename)
+
+    # TODO: may be need to validate src and trans lang
+    translated_file_url = generate_translated_file(
+        src_text=text,
+        src_lang=src_lang,
+        trans_lang=trans_lang,
+        filename=filename
+    )
+
+    return {
+        'file_url': translated_file_url,
+        'trans_lang': trans_lang,
+        'src_lang': src_lang
+    }
