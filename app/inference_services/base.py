db88236c714f3ceb3176c1b693396055c71312f2
ConradKash <kakurucon1234@gmail.com>
2023-09-01 00:38:15 +0300

    Translating Texts

--- app/inference_services/base.py
+++ app/inference_services/base.py
@@ -1,18 +1,10 @@
 import requests
 
+
 API_URL = "https://api-inference.huggingface.co/models/Sunbird/sunbird-en-mul"
 headers = {"Authorization": "Bearer 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"}
 
 
-def inference_request(payload):
+def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-
-
-output = inference_request(
-    {
-        "inputs": ">>lug<<The answer is Yes!"
-    }
-)
-
-print(output)
+    return response.text
