1755c9be288146b64d5eb287b959d32436931667
ConradKash <kakurucon1234@gmail.com>
2023-09-01 01:08:57 +0300

    Exponential Backoff

--- app/inference_services/base.py
+++ app/inference_services/base.py
@@ -1,10 +1,17 @@
 import requests
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 
 API_URL = "https://api-inference.huggingface.co/models/Sunbird/sunbird-en-mul"
 headers = {"Authorization": "Bearer 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"}
 
+# Define the retry decorator
 
+
+@retry(
+    stop=stop_after_attempt(5),  # Maximum number of retries
+    wait=wait_exponential(multiplier=1, min=1, max=60)  # Exponential backoff
+)
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.text
