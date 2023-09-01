45c939cd4092c80b613386750464069df3b8b7c5
ConradKash <kakurucon1234@gmail.com>
2023-09-01 14:53:46 +0300

    Mult - Mult Translation implemented

--- app/inference_services/translate.py
+++ app/inference_services/translate.py
@@ -1,19 +1,39 @@
-from base import query
+from base import inference_request_en_mul, inference_request_mul_en
 
 
-def create_payload(text, target_language):
+def create_payload_en_mul(text, target_language):
     payload = {
         "inputs": ">>" + str(target_language) + "<<" + str(text)
     }
     return payload
 
 
-def translate(text, target_language=None):
-    payload = create_payload(text,
-                             target_language)
-    response = query(payload)
+def create_payload_mul_en(text, target_language):
+    payload = {
+        "inputs": ">>" + str(target_language) + "<<" + str(text)
+    }
+    return payload
+
+
+def translate(text, source_language=None, target_language=None):
+    if source_language != 'eng' and target_language != 'eng':
+        payload = create_payload_mul_en(text,
+                                        source_language)
+        response = inference_request_mul_en(payload)
+        response = response[20:-3]
+        payload = create_payload_en_mul(response,
+                                        target_language)
+        response = inference_request_en_mul(payload)
+    elif source_language == 'eng':
+        payload = create_payload_en_mul(text,
+                                        target_language)
+        response = inference_request_en_mul(payload)
+    else:
+        payload = create_payload_mul_en(text,
+                                        source_language)
+        response = inference_request_mul_en(payload)
     response = response[20:-3]
     return response
 
 
-print(translate("Where are we", "nyn"))
+print(translate("Nituza ahari nyikiro", "nyn", "lug"))
