26a3707705f11783e2c4c858f97de12defa952e6
ConradKash <kakurucon1234@gmail.com>
2023-09-01 17:19:52 +0300

    Language-ID is active where source language == None

---
+++ app/inference_services/language_ID_Impl/model.py
@@ -0,0 +1,12 @@
+import joblib
+
+with open("app/inference_services/language_ID_Impl'\
+          '/pipeline_MultinomialNB.pkl", "rb") as f:
+    model = joblib.load(f)
+
+
+def predicted_language(sentence):
+    samples = [sentence]
+    prediction = str(model.predict(samples))
+    prediction = prediction[2:-2]
+    return prediction
--- app/inference_services/translate.py
+++ app/inference_services/translate.py
@@ -1,4 +1,5 @@
 from base import inference_request_en_mul, inference_request_mul_en
+from language_ID_Impl.model import predicted_language
 
 
 def create_payload_en_mul(text, target_language):
@@ -8,32 +9,43 @@ def create_payload_en_mul(text, target_language):
     return payload
 
 
-def create_payload_mul_en(text, target_language):
+def create_payload_mul_en(text):
     payload = {
-        "inputs": ">>" + str(target_language) + "<<" + str(text)
+        "inputs": str(text)
     }
     return payload
 
 
-def translate(text, source_language=None, target_language=None):
-    if source_language != 'eng' and target_language != 'eng':
-        payload = create_payload_mul_en(text,
-                                        source_language)
-        response = inference_request_mul_en(payload)
+def translate(text, target_language=None, source_language=None):
+    def translate_text():
+        if source_language != 'eng' and target_language != 'eng':
+            payload = create_payload_mul_en(text)
+            response_eng = inference_request_mul_en(payload)
+            response_eng = response_eng[20:-3]
+            payload = create_payload_en_mul(response_eng,
+                                            target_language)
+            response = inference_request_en_mul(payload)
+            print("mult-mult")
+
+        elif source_language == 'eng':
+            payload = create_payload_en_mul(text,
+                                            target_language)
+            response = inference_request_en_mul(payload)
+            print("eng-mult")
+
+        elif target_language == 'eng':
+            payload = create_payload_mul_en(text)
+            response = inference_request_mul_en(payload)
+            print("mult-eng")
         response = response[20:-3]
-        payload = create_payload_en_mul(response,
-                                        target_language)
-        response = inference_request_en_mul(payload)
-    elif source_language == 'eng':
-        payload = create_payload_en_mul(text,
-                                        target_language)
-        response = inference_request_en_mul(payload)
+        return response
+
+    if source_language is None:
+        source_language = predicted_language(text)
+        response_text = translate_text()
     else:
-        payload = create_payload_mul_en(text,
-                                        source_language)
-        response = inference_request_mul_en(payload)
-    response = response[20:-3]
-    return response
+        response_text = translate_text()
+    return response_text
 
 
-print(translate("Nituza ahari nyikiro", "nyn", "lug"))
+print(translate("Tudda eka leero", "nyn", None))
