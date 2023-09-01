b0f1decff0d8236605c4481a30b8c16e05c92c8e
ConradKash <kakurucon1234@gmail.com>
2023-09-01 17:19:52 +0300

    Language-ID is active where source language == None

--- app/inference_services/base.py
+++ app/inference_services/base.py
@@ -1,26 +1,30 @@
 import requests
+import os
+from dotenv import load_dotenv
 from tenacity import retry, stop_after_attempt, wait_exponential
+
+load_dotenv()
 
 
 @retry(
-    stop=stop_after_attempt(5),  # Maximum number of retries
+    stop=stop_after_attempt(4),  # Maximum number of retries
     wait=wait_exponential(multiplier=1, min=1, max=60)  # Exponential backoff
 )
 def inference_request_en_mul(payload):
     API_URL = 'https://api-inference.huggingface.co/m'\
               'odels/Sunbird/sunbird-en-mul'
-    headers = {"Authorization": "Bearer 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"}
-    response = requests.post(API_URL, headers=headers, json=payload)
+    headers_en_mul = {"Authorization": os.getenv("HEADERS_EN_MUL")}
+    response = requests.post(API_URL, headers=headers_en_mul, json=payload)
     return response.text
 
 
 @retry(
-    stop=stop_after_attempt(5),  # Maximum number of retries
+    stop=stop_after_attempt(4),  # Maximum number of retries
     wait=wait_exponential(multiplier=1, min=1, max=60)  # Exponential backoff
 )
 def inference_request_mul_en(payload):
     API_URL = 'https://api-inference.huggingface.co/m'\
               'odels/Sunbird/mbart-mul-en'
-    headers = {"Authorization": "Bearer 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"}
-    response = requests.post(API_URL, headers=headers, json=payload)
+    headers_mul_en = {"Authorization": os.getenv("HEADER_MUL_EN")}
+    response = requests.post(API_URL, headers=headers_mul_en, json=payload)
     return response.text
