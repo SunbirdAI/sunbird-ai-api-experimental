3f84d2a3ab99aba0d56218ef8349c7d7ec8f6f7f
ConradKash <kakurucon1234@gmail.com>
2023-09-01 14:53:46 +0300

    Mult - Mult Translation implemented

--- app/inference_services/base.py
+++ app/inference_services/base.py
@@ -2,16 +2,25 @@ import requests
 from tenacity import retry, stop_after_attempt, wait_exponential
 
 
-API_URL = "https://api-inference.huggingface.co/models/Sunbird/sunbird-en-mul"
-headers = {"Authorization": "Bearer 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"}
-
-# Define the retry decorator
+@retry(
+    stop=stop_after_attempt(5),  # Maximum number of retries
+    wait=wait_exponential(multiplier=1, min=1, max=60)  # Exponential backoff
+)
+def inference_request_en_mul(payload):
+    API_URL = 'https://api-inference.huggingface.co/m'\
+              'odels/Sunbird/sunbird-en-mul'
+    headers = {"Authorization": "Bearer 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"}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.text
 
 
 @retry(
     stop=stop_after_attempt(5),  # Maximum number of retries
     wait=wait_exponential(multiplier=1, min=1, max=60)  # Exponential backoff
 )
-def query(payload):
+def inference_request_mul_en(payload):
+    API_URL = 'https://api-inference.huggingface.co/m'\
+              'odels/Sunbird/mbart-mul-en'
+    headers = {"Authorization": "Bearer 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"}
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.text
