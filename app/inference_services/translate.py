07b6429f165a22de5d47836c51f645981813f895
ConradKash <kakurucon1234@gmail.com>
2023-09-02 16:29:00 +0300

    Validation and Batch Translation Implemented

--- app/inference_services/translate.py
+++ app/inference_services/translate.py
@@ -22,30 +22,72 @@ def translate(text, target_language=None, source_language=None):
             payload = create_payload_mul_en(text)
             response_eng = inference_request_mul_en(payload)
             response_eng = response_eng[20:-3]
+            print(response_eng)
             payload = create_payload_en_mul(response_eng,
                                             target_language)
             response = inference_request_en_mul(payload)
-            print("mult-mult")
 
         elif source_language == 'eng':
             payload = create_payload_en_mul(text,
                                             target_language)
             response = inference_request_en_mul(payload)
-            print("eng-mult")
 
         elif target_language == 'eng':
             payload = create_payload_mul_en(text)
             response = inference_request_mul_en(payload)
-            print("mult-eng")
         response = response[20:-3]
         return response
 
-    if source_language is None:
-        source_language = predicted_language(text)
-        response_text = translate_text()
-    else:
-        response_text = translate_text()
-    return response_text
+    while True:
+        try:
+            if (len(text) > 3 and len(text) < 5000):
+                if source_language is None:
+                    source_language = predicted_language(text)
+                    response_text = translate_text()
+                else:
+                    response_text = translate_text()
+                print('Please')
+            return response_text
+        except ValueError:
+            response_text = ("Please enter a valid text longer than "
+                             "3 characters and less than 5000 characters")
+            return response_text
 
 
-print(translate("Tudda eka leero", "nyn", None))
+print(translate("Tugenda waa", "nyn", 'lug'))
+
+
+def create_chunks(text: str, chunk_size: int):
+    # TODO: currently chunk size has to be larger than first word in text
+    chunks = []
+    last_char_index = len(text)
+    chunk_start = 0
+    chunk_stop = 0
+
+    while chunk_stop != last_char_index:
+
+        chunk_stop += chunk_size
+
+        if chunk_stop > last_char_index:
+            chunk_stop = last_char_index
+
+        if chunk_stop != last_char_index:
+            while text[chunk_stop] != " ":
+                chunk_stop -= 1
+
+        chunks.append(text[chunk_start:chunk_stop])
+
+        chunk_start = chunk_stop+1
+
+    return chunks
+
+
+def batch_text_translation(src_text: str, src_lang: str, trans_lang: str):
+    src_text_chunks = create_chunks(src_text, chunk_size=200)
+    trans_text_chunks = []
+    for chunk in src_text_chunks:
+        trans_text_chunks.append(
+                         translate(text=chunk,
+                                   target_language=trans_lang,
+                                   source_language=src_lang))
+    return " ".join(trans_text_chunks)
