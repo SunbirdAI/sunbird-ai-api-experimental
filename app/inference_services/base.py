3de6f5778b06682a193e2b2e9113721609b2a6da
ConradKash <kakurucon1234@gmail.com>
2023-09-05 21:01:38 +0300

    Exponnential Backoff successfully implemented

--- app/inference_services/base.py
+++ app/inference_services/base.py
@@ -3,19 +3,20 @@ import os
 import time
 from dotenv import load_dotenv
 from tenacity import retry, wait_exponential
-import backoff
 
 load_dotenv()
 
 
-@backoff.on_exception(backoff.expo,
-                      requests.exceptions.RequestException,
-                      max_time=100)
+@retry(
+    wait=wait_exponential(multiplier=3, min=98, max=120)  # Exponential backoff
+)
 def inference_request_en_mul(payload):
     url = 'https://api-inference.huggingface.co/m'\
               'odels/Sunbird/sunbird-en-mul'
     headers_en_mul = {"Authorization": os.getenv("HEADER_HUGGING_FACE_TOKEN")}
     response = requests.post(url, headers=headers_en_mul, json=payload)
+    # TODO Create a function that just calls it
+    # This is where i applied the exponential backoff
     if response.status_code == 503:
         estimated_time = response.json()['estimated_time']
         time.sleep(estimated_time)
@@ -35,6 +36,7 @@ def inference_request_mul_en(payload):
               'odels/Sunbird/mbart-mul-en'
     headers_mul_en = {"Authorization": os.getenv("HEADER_HUGGING_FACE_TOKEN")}
     response = requests.post(url, headers=headers_mul_en, json=payload)
+    # This is where i applied the exponential backoff
     if response.status_code == 503:
         estimated_time = response.json()['estimated_time']
         time.sleep(estimated_time)
