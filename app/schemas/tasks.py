6e03ce45fbd9185ad47d67e5dc5db8ac66212cb3
ConradKash <kakurucon1234@gmail.com>
2023-09-06 00:53:54 +0300

    Source language detect

--- app/schemas/tasks.py
+++ app/schemas/tasks.py
@@ -13,10 +13,10 @@ from pydantic import BaseModel, Field
 
 class TranslationResponse(BaseModel):
     text: str
-    source_language: str | None = None
+    source_language: str = None
 
 
 class TranslationRequest(BaseModel):
-    source_language: str | None = None
+    source_language: str = None
     target_language: str
     text: str = Field(max_length=5000, min_length=3)
