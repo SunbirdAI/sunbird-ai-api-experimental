b7b8b88a751fa219fb8fcbfd9bcdb591f77efca0
ConradKash <kakurucon1234@gmail.com>
2023-09-08 02:36:16 +0300

    No module Error

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,8 +1,7 @@
 from fastapi.testclient import TestClient
 from app.api import app
-import json
 from unittest.mock import MagicMock
-from app.inference_services.translate import translate_text,create_payload_en_mul,inference_request_en_mul,predicted_language
+from app.inference_services.translate import translate_text, predicted_language
 client = TestClient(app)
 
 
@@ -14,29 +13,34 @@ def test_root_api():
 
 def test_mul_mul(mocker):
     fake_response_eng = MagicMock()
-    fake_response_eng = [{"generated_text":"Where are we heading?"}]
-    mocker.patch('app.inference_services.base.inference_request_mul_en', return_value=fake_response_eng)
+    fake_response_eng = [{"generated_text": "Where are we heading?"}]
+    mocker.patch('app.inference_services.base.inference_request_mul_en.reque'
+                 'sts.post').return_value.text.return_value = fake_response_eng
 
     fake_response_mul = MagicMock()
-    fake_response_mul = [{"generated_text":"Nituza nkahi?"}]
-    mocker.patch('app.inference_services.base.inference_request_en_mul', return_value=fake_response_mul)
+    fake_response_mul = [{"generated_text": "Nituza nkahi?"}]
+    mocker.patch('app.inference_services.base.inference_request_en_mul.reque'
+                 'sts.post').return_value.text.return_value = fake_response_mul
 
     assert translate_text('Tuli wa', 'lug', 'nyn') == 'Nituza nkahi?'
 
 
-
 def test_mul_eng(mocker):
-    fake_response_eng = [{"generated_text":"Where are we heading?"}]
-    mocker.patch('app.inference_services.base.inference_request_mul_en', return_value=fake_response_eng)
-    # mocker.patch('app.inference_services.base.inference_request_mul_en.requests.post').return_value.text.return_value = fake_response_eng
+    fake_response_eng = [{"generated_text": "Where are we heading?"}]
+    mocker.patch('app.inference_services.base.inference_request_mul_en.reque'
+                 'sts.post').return_value.text.return_value = fake_response_eng
 
-    assert translate_text('Tuli wa', 'lug', 'eng') == "Where are we heading?"
+    assert translate_text('Tuli wa', 'lug',
+                          'eng') == "Where are we heading?"
+
 
 def test_eng_mul(mocker):
-    fake_response_mul = [{"generated_text":"Nituza nkahi?"}]
-    mocker.patch('app.inference_services.base.inference_request_en_mul', return_value=fake_response_mul)
+    fake_response_mul = [{"generated_text": "Nituza nkahi?"}]
+    mocker.patch('app.inference_services.base.inference_request_mul_en.reque'
+                 'sts.post').return_value.text.return_value = fake_response_mul
 
-    assert translate_text('Where are we heading?', 'eng', 'nyn') == "Nituza nkahi?"
+    assert translate_text('Where are we heading?',
+                          'eng', 'nyn') == "Nituza nkahi?"
 
 
 def test_language_detect():
